ec1c537b317b7373dca535d310c88746eb389ab2
Md Musharraf Khan <mdmusharrafk@gmail.com>
2025-11-13 17:32:13 +0530

    changes done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
           }
         });
       },
-      { threshold: 0.7 }
+      { threshold: 0.5 }
     );
 
     sections.forEach((sec) => observer.observe(sec));
